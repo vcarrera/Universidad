6bf3f6a6ee6f7e3ed254f6080d072c23bd3ccb78
Victor carrera <v.carrera@alumnos.urjc.es>
2015-01-30 17:25:49 +0100

    practica android funcionando hasta juego nuevo

--- Android/p1-memoria/GUI.java
+++ Android/p1-memoria/GUI.java
@@ -39,24 +39,6 @@ public class GUI {
         }
     }
 
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_pirates:
-                if (checked)
-                    // Pirates are the best
-                    break;
-            case R.id.radio_ninjas:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
-    }
-
-
     private class Infobut implements View.OnClickListener {
         private Integer id;
         private Button button;
